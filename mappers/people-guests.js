7ea65b87f2eec5a85cd0d129d1e548ac53864704
R. Allan White <allan@catalyze.io>
2016-11-22 19:05:57 -0800

    Let’s play with YAML

--- mappers/people-guests.js
+++ mappers/people-guests.js
@@ -5,10 +5,11 @@ module.exports = function(file) {
     //Replace 'Title' with 'title', 'SeoTitle' with 'seoTitle', etc.
     keys.forEach(function(key) {
         var field = file.content.fields[key];
+        console.log('field name: ' + field);
         // var newKey = key.charAt(0).toLowerCase() + key.slice(1);
         var newKey = key.toLowerCase();
         delete file.content.fields[key];
-        file.content.fields[newKey] = field;
+        file.content.fields[key] = field;
         // console.log('new field name: ' + newKey);
     });
 
@@ -17,12 +18,20 @@ module.exports = function(file) {
     //  To remove it:
     //delete file.content.fields.seotitle;
     //  To rename it (renaming to 'seoTitle' in this example):
-    //file.content.fields.seoTitle = file.content.fields.seotitle;
-    //delete file.content.fields.seotitle;
+    // file.content.fields.Bio = file.content.fields.bio;
+    // delete file.content.fields.bio;
 
+    // file.content.fields.bioShort = file.content.fields.bioshort;
+    // delete file.content.fields.bioshort;
     //Field removal
-    file.content.fields.title = file.content.fields.fullname;
-    delete file.content.fields.fullname;
+    // file.content.fields.Title = file.content.fields.fullname;
+    delete file.content.fields.Title;
+
+    delete file.content.fields.body;
+    delete file.content.fields.GuestAvatar;
+
+    // file.content.fields.personType = file.content.fields.persontype;
+    // delete file.content.fields.persontype;
 
     //Field renaming
     // file.content.fields.tagString = file.content.fields.tags;
@@ -34,8 +43,13 @@ module.exports = function(file) {
     // var tagStr = file.content.fields.tags['en-US'];
     // var tagArray = tagStr.split(', ');
     // console.log('tagArray: ' + tagArray);
-
-    // console.log(file);
+    var pathParts = file.path.split('/');
+    var slugParts = pathParts[pathParts.length-2].split('-');
+    var slug = slugParts.slice(1).join('-');
+    file.content.fields.slug = {
+        'en-US': slug
+    };
+    console.log(file);
 
     return file;
 };
